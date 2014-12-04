af288f90119966a3fb7f65c1e382eeaa191d2033
MatthewDaws <matt.daws@cantab.net>
2014-12-04 21:57:49 +0000

    Fixed openmp scheduling
    Also implemented a PRND (to get a thread-safe version; little difference
    in speed).

--- benchmarking/Parallelising/Parallel(shared_memory)_MC_calculation_of_pi1.c
+++ benchmarking/Parallelising/Parallel(shared_memory)_MC_calculation_of_pi1.c
@@ -4,6 +4,8 @@
 // 2 Dec 14: Matt Daws added timing code and a single threaded test function
 //           and added random number seed setting.
 //   omp_set_num_threads should not be in #prgama block.
+// 4 Dec 14: Matt Daws changed rand() to thread-safe version.
+//   Doesn't seem to help in making the multi-threaded version competative in speed!
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +13,38 @@
 
 #include <sys/time.h>
 
+// Mingw-64 at least does not implement this function
+// This implements a 32-bit linear congruence generator; this is not a good
+// type of PRND, but will suffice as proof of concept.
+struct drand48_data {
+	unsigned int X;
+};
+int drand48_r(struct drand48_data *buf, double *d)
+{
+	unsigned long long Y = 1103515245;
+	Y = Y * ((unsigned long long)buf->X) + 12345;
+	buf->X = (Y & 2147483647);
+	*d = ((double)buf->X) / 2147483647.0;
+	return 0;
+}
+int erand48_r(unsigned short int xsubi[3], struct drand48_data *buf, double *d)
+{
+	buf->X = xsubi[0];
+	buf->X += ((unsigned int)xsubi[1]) << 16;
+	return drand48_r(buf, d);
+}
+
 float get_pi_single(int N)
 {
   int pi=0,n;
-  float x,y;
+  double x,y;
+  struct drand48_data drand_buf;
+  unsigned short int seed[3] = {1,2,3};
+  erand48_r(seed, &drand_buf, &x);
   for(n=0;n<N;++n){
-    x=((float)rand())/((float)RAND_MAX);
-    y=((float)rand())/((float)RAND_MAX);
-    if(x*x+y*y<=1.0){
-      ++pi;
-    }
+    drand48_r(&drand_buf, &x);
+    drand48_r(&drand_buf, &y);
+    if (x*x+y*y<=1.0) { ++pi; }
   }
   return 4.0*((float)pi)/((float)N);
 }
@@ -28,14 +52,18 @@ float get_pi_single(int N)
 float get_pi(int N){
   //Monte Carlo method of calculating pi the argument is the number of samples to use.
   int pi=0,n;
-  float x,y;
-#pragma omp parallel private(n,x,y)reduction(+:pi)//enter parallel region of code
+  double x,y;
+  struct drand48_data drand_buf;
+  unsigned short int seed[3] = {1,2,3};
+  erand48_r(seed, &drand_buf, &x);
+#pragma omp parallel private(n,x,y,drand_buf)reduction(+:pi)//enter parallel region of code
 {
 #pragma omp for schedule(static)//collapse loop ('static' seems to work; 'dynamic' does not...)
-  for(n=0;n<N;++n){
-    x=((float)rand())/((float)RAND_MAX);
-    y=((float)rand())/((float)RAND_MAX);
+  for(n=0;n<N;++n) {
+    drand48_r(&drand_buf, &x);
+    drand48_r(&drand_buf, &y);
     if(x*x+y*y<=1.0){
+      #pragma omp atomic
       ++pi;
     }
   }
@@ -55,7 +83,6 @@ int main(int argc,char *argv[])
   omp_set_num_threads(cores);//set the number of threads
   struct timeval t1, t2;
   gettimeofday(&t1,0);
-  srand(12345);
   printf("%f\n",get_pi(N));
   gettimeofday(&t2,0);
   printf("Start time: %ld secs, %ld msecs.\n", t1.tv_sec, t1.tv_usec);
@@ -65,7 +92,6 @@ int main(int argc,char *argv[])
   printf("Elapsed time: %f.\n", dt2-dt1);
 
   gettimeofday(&t1,0);
-  srand(12345);
   printf("%f\n",get_pi_single(N));
   gettimeofday(&t2,0);
   dt1 = t1.tv_sec + ((double)t1.tv_usec)/1000000.0;
@@ -73,7 +99,6 @@ int main(int argc,char *argv[])
   printf("Single Thread time: %f.\n", dt2-dt1);
 
   gettimeofday(&t1,0);
-  srand(12345);
   printf("%f\n",get_pi_single(N));
   gettimeofday(&t2,0);
   dt1 = t1.tv_sec + ((double)t1.tv_usec)/1000000.0;
